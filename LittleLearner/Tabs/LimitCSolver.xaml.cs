f41f2db4d85c46d94431417614c26afe3a36706a
Louis <louis.baldewein@web.de>
2026-02-17 14:58:02 +0100

    Automatische code generierung fast komplett implementiert

--- LittleLearner/Tabs/LimitCSolver.xaml.cs
+++ LittleLearner/Tabs/LimitCSolver.xaml.cs
@@ -1,25 +1,40 @@
 using LimitCSolver.LimitCGenerator;
-using System.Diagnostics;
+using System.Text.RegularExpressions;
 
 namespace LittleLearner.Tabs;
 
 public partial class LimitCSolver : ContentPage
 {
+	DifficultySettings? difficulty;
+
 	public LimitCSolver()
 	{
 		InitializeComponent();
 	}
 
-	public void toggleCodeCreator(){
-		Code.Text = "Hello World";
-		CodeCreator.IsVisible = !CodeCreator.IsVisible;
-	}
+	public void ToggleCodeCreator(object sender, EventArgs arguments){ CodeCreator.IsVisible = !CodeCreator.IsVisible; }
 
-	public void automaticCreateCode()
+	public void AutomaticCreateCode(object sender, EventArgs arguments)
 	{
-		Settings difficultySettings = new Settings();
-		CodeGenerator codeGenerator = new CodeGenerator(difficultySettings.Medium);
-		String code = codeGenerator.GenerateCode();
-		Code.Text = "Hello";
+		if(difficulty == null) { return; }
+		string createdCode = (new CodeGenerator(difficulty)).GenerateCode();
+		createdCode = createdCode.Replace("\\r\\n", "\n");
+		createdCode = Regex.Replace(createdCode, "^{\"Code\":\"", "");
+        createdCode = Regex.Replace(createdCode, "\\n\"}", "");
+
+        code.Text = createdCode;
+    }
+
+	public void SelectDifficulty(object sender, EventArgs arguments)
+	{
+		DifficultyEasy.BackgroundColor = new Color(255, 0, 255);
+        DifficultyMedium.BackgroundColor = new Color(255, 0, 255);
+        DifficultyHard.BackgroundColor = new Color(255, 0, 255);
+		
+		if(sender == DifficultyEasy){ difficulty = new Settings().Easy; }
+		else if(sender == DifficultyMedium){ difficulty = new Settings().Medium; }
+		else if (sender == DifficultyHard) { difficulty = new Settings().Hard; }
+
+        ((Border)sender).BackgroundColor = new Color(255, 0, 0);
 	}
 }
